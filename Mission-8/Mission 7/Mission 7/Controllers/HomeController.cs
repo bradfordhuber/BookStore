0cce933a75b9ce1fe9a86a2b05240613b1dcfd98
bradfordhuber <bradfordjhuber@gmail.com>
2022-02-25 22:14:24 -0700

    commit

---
+++ Mission-8/Mission 7/Mission 7/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission_7.Models;
+using Mission_7.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+
+            return View(x);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission-8/Mission 7/Mission 7/Models/BookstoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_7.Models
+{
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+
+
+}
